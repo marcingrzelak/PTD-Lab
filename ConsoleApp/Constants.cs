8c8944d7e48a20566fb9260e3175691c3359850b
Unknown <marcin.grzelak@op.pl>
2018-05-03 14:40:03 +0200

    Creating hierarchy of project
    3.05.18 14:40

---
+++ ConsoleApp/Constants.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Constants
+    {
+        public const byte ADDR_TORQUE_ENABLE = 24;
+        public const byte ADDR_LED_ENABLE = 25;
+        public const byte ADDR_GOAL_POSITION = 30;
+        public const byte ADDR_MOVING_SPEED = 32;
+        public const byte ADDR_TORQUE_LIMIT = 34;
+        public const byte ADDR_PRESENT_POSITION = 36;
+        public const byte ADDR_PRESENT_SPEED = 38;
+        public const byte ADDR_MOVING = 46;
+
+        public const byte FRONT_RIGHT_WHEEL = 4;
+        public const byte FRONT_LEFT_WHEEL = 2;
+        public const byte REAR_RIGTH_WHEEL = 3;
+        public const byte REAR_LEFT_WHEEL = 1;
+
+        // Protocol version
+        
+        public const int TORQUE_ENABLE = 1;                   // Value for enabling the torque
+        public const int TORQUE_DISABLE = 0;                   // Value for disabling the torque
+        
+        public const int COMM_SUCCESS = 0;                   // Communication Success result value
+        public const int COMM_TX_FAIL = -1001;               // Communication Tx Failed
+
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dynamixel_sdk;
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        #region Settings
+        public const byte PROTOCOL_VERSION = 1;
+        public const int BAUDRATE = 1000000;
+        public const string DEVICENAME = "COM1";
+        #endregion
+
+
+
+        static void OpenPort(int PortNum)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            int port_num = dynamixel.portHandler("COM1");
+        }
+    }
+}
